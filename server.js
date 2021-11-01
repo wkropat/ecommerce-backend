3f7d1d154deacda2dad77e9883af02e3ac77dc0a
wkropat <wkropat@gmail.com>
2021-11-01 15:27:06 -0700

    server built

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const app = express();
+// Heroku-friendly port
+const PORT = process.env.PORT || 3666;
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(routes);
+
+
+// Listen in
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('Listening in on https://' + PORT);
+    });
+});
